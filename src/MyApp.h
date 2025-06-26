cd4c9583999d7172917a0e12050868e596528c06
WagnerAndras <wagner.andris@gmail.com>
2025-06-26 02:05:20 +0200

    3D init distribution settings

--- src/MyApp.h
+++ src/MyApp.h
@@ -82,6 +82,8 @@ protected:
 	struct cudaGraphicsResource* world_matricesBO_CUDA;
 
 	// Variables
+	int m_distribution_idx = 0;
+
 	SteeringParams m_steering_params = {};
 	MovementParams m_movement_params = {};
 	
